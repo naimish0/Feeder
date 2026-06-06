55410a87888b64ebf67bee744fed9e2c8a8b25cc
naimishgupta <naimish.gupta@accedo.tv>
2026-06-06 08:05:25 +0530

    master: Initial Commit

---
+++ shared/src/commonMain/kotlin/com/example/feeder/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.feeder
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/example/feeder/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.feeder
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
