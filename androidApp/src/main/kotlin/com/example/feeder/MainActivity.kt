05cfb9f9d0d014b9856e44b156ac18a39d298937
naimishgupta <naimish.gupta@accedo.tv>
2026-06-12 01:30:06 +0530

    T11-Added-Splash-Screen: Added Splash And App Logo for Both Android And IOS.

--- androidApp/src/main/kotlin/com/example/feeder/MainActivity.kt
+++ androidApp/src/main/kotlin/com/example/feeder/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import feature_feed.presentation.viewmodel.FeedViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import core.security.SessionManager
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
-
+    private val sessionManager: SessionManager by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition {
+            sessionManager.sessionState.value == core.security.SessionState.Loading
+        }
+        lifecycleScope.launch {
+            sessionManager.initialize()
+        }
+
         setContent {
             App()
         }
